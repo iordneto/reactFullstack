f35bd8adadd9a4a813bdc03471800b2583d8e1b1
Iord Neto <iordneto95@gmail.com>
2018-01-25 10:45:42 -0200

    Projeto finalizado. Criado boões de ações, listagem, edição, remoção e conclusão de tarefas. Adicionado pesquisa por botão e tecla de atalho.

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -1,6 +1,8 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
+import '../template/custom.css'
+
 import React from 'react'
 import Routes from './routes'
 import Menu from '../template/menu'
--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
+import Axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
     constructor(props) {
@@ -10,14 +14,52 @@ export default class Todo extends Component {
         
         this.handleAdd = this.handleAdd.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
+        this.handleClear = this.handleClear.bind(this)
+
+        this.refresh()
+    }
+
+    refresh(description = '') {
+        const search = description ? `&description__regex=/${description}/` : '';
+        Axios.get(`${URL}?sort=-createdAt${search}`)
+            .then(resp => this.setState({...this.state, description, list: resp.data}))
+    }
+
+    handleSearch() {
+        this.refresh(this.state.description)
+    }
+
+    handleClear(){
+        this.refresh()
     }
 
     handleAdd() {
-        console.log(this.state)
+        const description = this.state.description
+        Axios.post(URL, { description })
+            .then(resp => this.refresh())
     }
 
     handleChange(e) {
         this.setState({...this.state, description: e.target.value})
+    }
+
+    handleRemove(todo) {
+        Axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh(this.state.description))
+    }
+
+    handleMarkAsDone(todo) {
+        Axios.put(`${URL}/${todo._id}`, {...todo, done: true})
+            .then(resp => this.refresh(this.state.description))
+    }
+
+    handleMarkAsPending(todo) {
+        Axios.put(`${URL}/${todo._id}`, {...todo, done: false})
+            .then(resp => this.refresh(this.state.description))
     }
 
     render(){
@@ -27,8 +69,16 @@ export default class Todo extends Component {
                 <TodoForm 
                     handleAdd={this.handleAdd} 
                     handleChange={this.handleChange} 
-                    description={this.state.description}/>   
-                <TodoList />             
+                    handleSearch={this.handleSearch}
+                    handleClear={this.handleClear}
+                    description={this.state.description} 
+                />   
+                <TodoList 
+                    list={this.state.list}
+                    handleRemove={this.handleRemove} 
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPending={this.handleMarkAsPending}
+                />             
             </div>
         )
     }
