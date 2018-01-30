f142796aac8e33ba101dc331644ad091bd11ca0c
Iord Neto <iordneto95@gmail.com>
2018-01-30 16:38:33 -0200

    Criada funcionalidade de pesquisar tarefas e finalizado a aplicação com React/Redux

--- todo-app-with-redux/frontend/src/todo/todoActions.js
+++ todo-app-with-redux/frontend/src/todo/todoActions.js
@@ -8,10 +8,11 @@ export const changeDescription = event => ({
 })
 
 export const search = () => {
-    const request = Axios.get(`${URL}?sort=-createdAt${search}`)
-    return {
-        type: "TODO_SEARCHED",
-        payload: request
+    return (dispatch, getState) => {
+        const description = getState().todo.description
+        const search = description ? `&description__regex=/${description}/` : '';
+        const request = Axios.get(`${URL}?sort=-createdAt${search}`)
+            .then(resp => dispatch({ type: "TODO_SEARCHED", payload: resp.data }))
     }
 }
 
@@ -45,5 +46,8 @@ export const remove = todo => {
 }
 
 export const clear = () => {
-    return { type: "TODO_CLEAR" }
+    return [
+        { type: "TODO_CLEAR" },
+        search()
+    ]
 }
