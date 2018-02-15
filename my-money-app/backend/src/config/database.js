27384326ff72f2e5f7f7ca453fe7015657b1f135
Iord Neto <iordneto95@gmail.com>
2018-02-15 13:31:24 -0200

    Alterações para deploy no Heroku

--- my-money-app/backend/src/config/database.js
+++ my-money-app/backend/src/config/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://localhost/mymoney', { useMongoClient: true })
+const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/mymoney'
+module.exports = mongoose.connect(url, { useMongoClient: true })
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o mínimo de '{MIN}'."
